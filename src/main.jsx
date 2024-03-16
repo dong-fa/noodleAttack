b78ef925d8018adb480de4aed376a4b048d40249
dong-fa <120082854+dong-fa@users.noreply.github.com>
2024-03-16 18:03:13 +0900

    프로젝트 세팅
    라우터 설정
    절대경로 설정
    폴더 설정

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,11 @@
-import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App.jsx";
 import { Reset } from "styled-reset";
+import App from "./App";
+import { Fragment } from "react";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
+  <Fragment>
     <Reset />
     <App />
-  </React.StrictMode>
+  </Fragment>
 );
---
+++ src/pages/Test.jsx
@@ -0,0 +1,11 @@
+import { Logo } from "@/assets";
+
+const Test = () => {
+  return (
+    <div>
+      <Logo />
+    </div>
+  );
+};
+
+export default Test;
